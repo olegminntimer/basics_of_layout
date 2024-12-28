cf448b2e912277cc5cef665d4215dbe2f1045f12
Oleg Timer... <olegtimer@yandex.ru>
2024-12-28 12:17:04 +0500

    add home.html

--- main.py
+++ main.py
@@ -1,1 +1,77 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_index(self):
+        return """
+            <!doctype html>
+            <html lang="ru">
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <title>Bootstrap demo</title>
+                <link href="css/bootstrap.min.css" rel="stylesheet"/>
+              </head>
+              <body>
+                <h1>Hello, world!</h1>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+              </body>
+            </html>
+        """
+
+    def __get_article_content(self, page_address):
+        if page_address == 'news1':
+            return 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. '
+        elif page_address == 'news2':
+            return 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'
+        elif page_address == 'news3':
+            return 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+
+        return 'Article not found!'
+
+    def __get_blog_article(self, page_address):
+        return f"""
+        <html><head>
+        <title>Blog</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        </head><body class="container">
+        <div class="card">
+        <div class="card-header">
+        <a class="btn btn-primary" href="/">Back</a>
+        </div>
+        <div class="card-body">
+        <p>{self.__get_article_content(page_address)}</p>
+        </div>
+        </div>
+        </body>
+        </html>
+        """
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        page_address = query_components.get('page')
+        page_content = self.__get_index()
+        if page_address:
+            page_content = self.__get_blog_article(page_address[0])
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
